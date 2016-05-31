cd016b3af0caee6dfe7d54cffbfc84e058ae8c90
Andrew McLeod <andrewclimbing@gmail.com>
2016-05-31 13:49:09 +0000

    Overhauled. Loads customers and addresses, not overwriting old customers.
    Needs a new custom field in 'Customers' - ebay_user_id.
    All checking (for customers) is based on ebay_user_id; duplicates are possible
    if any Ebay buyers are already in the system. Addresses are compared by exact
    string comparison on non-empty entries of 'address_line1', 'address_line2',
    'city' and 'pincode' (postcode). If a customer does not have a name, their
    ebay_buyer_id is used instead. This is NOT corrected later if their actual
    name is later found.
    
    Some data sanitizing occurs before data from Ebay reaches the system:
    - Names are tidied up (Title Case) if they are either entirely lower or
        upper case, and contain at least one space character.
    - Postcodes are made uppercase and correct spacing introduced.
    
    To do: correct Customer customer_name if found in later order
           check for customers not on Ebay, and add ebay_buyer_id

--- erpnext_ebay/ebay_requests.py
+++ erpnext_ebay/ebay_requests.py
@@ -1,184 +1,51 @@
+"""Functions to retrieve data from eBay via ebaysdk module and TradingAPI"""
+
 from __future__ import unicode_literals
-import frappe
-from frappe import _
-import json, math, time
-#from .exceptions import ShopifyError
-from frappe.utils import get_request_session
-
-
-
-
 import os
 import sys
-import datetime
-import csv
-from optparse import OptionParser
+#import datetime
 
-#sys.path.insert(0, '%s/../' % os.path.dirname(__file__))
+import frappe
+from frappe import _
 
-from common import dump
-
-#sys.path.insert(0, "/Users/macmini/frappe-bench/apps/unigreenscheme/ebaysdk-python-master")
-#sys.path.append("/Users/macmini/frappe-bench/apps/unigreenscheme/ebaysdk-python-master")
-#sys.path.append("/Users/macmini/Documents/dev/ebaysdk-python-master")
-sys.path.append("/home/frappe/frappe-bench/apps/erpnext_ebay/ebaysdk-python-master")
-sys.path.append("/Library/Python/2.7/site-packages")
-
-import ebaysdk
-from ebaysdk.utils import getNodeText
 from ebaysdk.exception import ConnectionError
 from ebaysdk.trading import Connection as Trading
 
 
-def get_ebay_customers(ignore_filter_conditions=False):
-	ebay_customers = []
-	filter_condition = ''
-	
-	if not ignore_filter_conditions:
-		filter_condition = None #get_filtering_condition()
-    
-        ebay_customers = get_orders()
-	return ebay_customers
-    
-    
-
-
 def get_orders():
-    
+    """Returns a list of recent orders from the Ebay TradingAPI"""
+
     orders = None
     ebay_customers = []
-    
-    CreateTimeFrom = str(datetime.date.today())
-    CreateTimeFrom = CreateTimeFrom+'T0:0:0.000Z'
-    CreateTimeTo = str(datetime.datetime.now())
-    CreateTimeTo = CreateTimeTo[0:len(CreateTimeTo)-3]+'Z'
-    
+
+    #CreateTimeFrom = str(datetime.date.today())
+    #CreateTimeFrom = CreateTimeFrom+'T0:0:0.000Z'
+    #CreateTimeTo = str(datetime.datetime.now())
+    #CreateTimeTo = CreateTimeTo[0:len(CreateTimeTo)-3]+'Z'
+
+    orders = []
+    page = 1
     try:
-        #api = Trading(debug=opts.debug, config_file=opts.yaml, appid=opts.appid,
-        #              certid=opts.certid, devid=opts.devid, warnings=True, timeout=20)
-        
-        
-        api = Trading(appid='Q20967060-98d1-42f3-8870-95ac890c9dc',devid='abc5d5bb-4394-4b2e-b587-d5f1f1aa9784', certid='3a530102-f035-4215-8c0b-6e81f54b655c',token='AgAAAA**AQAAAA**aAAAAA**2Q3WVg**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6wNloGkCZCDoAudj6x9nY+seQ**FegAAA**AAMAAA**wXWh4nAKbWaTigoWvCN1zxd18pMiJPNhL3eCRYKYDvJticp7UJIwcI8ruTeuyRSVAIPREd4E2f56vIvz84u9Q5mMxUYD5OKI+g8E0yPY3/onyc621vAVcMzi9GGZMvA5Yti45m+eK9NbsuZ40N6rA+AYICkYrYoH72+auU1VzuNrY95NhEkoe79hoa+MWBTXBc6OyGlRoQYPmL2LLpSILdmaYuoc14qBJyyjHIHpmBStA11G6fmrpw4pHxWNUfn434OFjvApXBbShgE7cmhV2HseaDV/jBOU7K1RkpUFXeev4RF2zPl+LSR/+sfsgHwfzRd0XK3mM4s8FIoXwlB27KkzIQCxvVIu2/rAMvasC/PUMSNo07cVYVuj3DG4tYLHywoKod0TM/TlwhqTd4k1kxvyoE7oNzde3hpo8Drhyl6ayzLPu4J97X3B+2I7SiVYVoBpFqHKxsbmGlQznZqxba1QyO/T2yEXORABQ4sOnb+neSnyQLTELEvYwWye6oKAW1Rg9PtAHxEpKuQpEvOUxkmIKEzXc0AEv5Z/lsnR8kNY5uJp9UAHv3p6whbgG3ntnGG/I7fFWETuNdwHJk1EhdOMLTCyRwT6oE630EtdjIYM86kGufeqBUEVG0VZGBUK1rJkEpmzHCaZoOm2+EtYim+HRZq964TkMc0EugLTlu3EvZPDybwSMyZdbP9f/HD+zrDm6KbuNayuzDfQ2XaVV1IplcbAddLQOC5NEo7ao5xlRjxmls7Zx5h+Nl0E5Mhu', config_file=None)
-        
-        api.execute('GetOrders', {'NumberOfDays': 30})
-        #dump(api, full=False)
-        
-        dom    = api.response_dom()
-        orders = api.response_dict()
-        myobj  = api.response_obj()
-        #orders = dom.getElementsByTagName('OrderArray')
-        #orders = mydict['OrderArray']['Order']
-        #print 'num of orders %s' % len(orders)
-        #for order in orders:
-            #print nodeText(item.getElementsByTagName('Order')[0])
-            #print order['CheckoutStatus']['Status']['value']
-        
-        '''if int(orders.ReturnedOrderCountActual) > 0:
-            if isinstance(orders.OrderArray.Order,list):
-                for order in orders.OrderArray.Order:
-                    ebay_customers.append([order.BuyerUserID, order.ShippingAddress.CityName, order.ShippingAddress.CountryName, order.ShippingAddress.Name, order.ShippingAddress.Phone, order.ShippingAddress.PostalCode, order.ShippingAddress.StateOrProvince, order.ShippingAddress.Street1, order.ShippingAddress.Street2])
-    
-    # order.OrderID, order.orderStatus, order.CheckoutStatus.Status, order.PaidTime, order.CreatedTime, order.Total, order.TransactionArray, order.TransactionArray.Transaction.ItemID, order.TransactionArray.Transaction.Item.SKU
-        '''
+        # Initialize TradingAPI; default timeout is 20.
+        api = Trading(config_file='ebay.yaml', warnings=True, timeout=20)
+        while True:
+            # TradingAPI results are paginated, so loop until
+            # all pages have been obtained
+            api.execute('GetOrders', {'NumberOfDays': 30,
+                                      'Pagination': {'EntriesPerPage': 100,
+                                                     'PageNumber': page}})
+
+            orders_api = api.response.dict()
+
+            if int(orders_api['ReturnedOrderCountActual']) > 0:
+                orders.extend(orders_api['OrderArray']['Order'])
+            if orders_api['HasMoreOrders'] == 'false':
+                break
+            page += 1
+
     except ConnectionError as e:
         print(e)
         print(e.response.dict())
-    
-    if int(orders.ReturnedOrderCountActual) > 0:
-        return orders
-    else:
-        return None
-        
-        
-        
-        
-        
-''''        
-def get_ebay_items(ignore_filter_conditions=False):
-	ebay_products = []
-	
-	filter_condition = ''
-	if not ignore_filter_conditions:
-		filter_condition = get_filtering_condition()
-	
-	for page_idx in xrange(0, get_total_pages("products", ignore_filter_conditions) or 1):
-		ebay_products.extend(get_request('/admin/products.json?limit=250&page={0}&{1}'.format(page_idx+1, 
-			filter_condition))['products'])
-            
-    
-			
-	return ebay_products
-    
-'''
+        raise e
 
-    
-
-    
-
-def verifyAddItemErrorCodes(opts):
-    """http://www.utilities-online.info/xmltojson/#.UXli2it4avc
-    """
-
-    try:
-        api = Trading(debug=opts.debug, config_file=opts.yaml, appid=opts.appid,
-                      certid=opts.certid, devid=opts.devid, warnings=False)
-
-        myitem = {
-            "Item": {
-                "Title": "Harry Potter and the Philosopher's Stone",
-                "Description": "This is the first book in the Harry Potter series. In excellent condition!",
-                "PrimaryCategory": {"CategoryID": "377aaaaaa"},
-                "StartPrice": "1.0",
-                "CategoryMappingAllowed": "true",
-                "Country": "US",
-                "ConditionID": "3000",
-                "Currency": "USD",
-                "DispatchTimeMax": "3",
-                "ListingDuration": "Days_7",
-                "ListingType": "Chinese",
-                "PaymentMethods": "PayPal",
-                "PayPalEmailAddress": "tkeefdddder@gmail.com",
-                "PictureDetails": {"PictureURL": "http://i1.sandbox.ebayimg.com/03/i/00/30/07/20_1.JPG?set_id=8800005007"},
-                "PostalCode": "95125",
-                "Quantity": "1",
-                "ReturnPolicy": {
-                    "ReturnsAcceptedOption": "ReturnsAccepted",
-                    "RefundOption": "MoneyBack",
-                    "ReturnsWithinOption": "Days_30",
-                    "Description": "If you are not satisfied, return the item for a refund.",
-                    "ShippingCostPaidByOption": "Buyer"
-                },
-                "ShippingDetails": {
-                    "ShippingType": "Flat",
-                    "ShippingServiceOptions": {
-                        "ShippingServicePriority": "1",
-                        "ShippingService": "USPSMedia",
-                        "ShippingServiceCost": "2.50"
-                    }
-                },
-                "Site": "US"
-            }
-        }
-
-        api.execute('VerifyAddItem', myitem)
-
-    except ConnectionError as e:
-        # traverse the DOM to look for error codes
-        for node in api.response.dom().findall('ErrorCode'):
-            print("error code: %s" % node.text)
-
-        # check for invalid data - error code 37
-        if 37 in api.response_codes():
-            print("Invalid data in request")
-
-        print(e)
-        print(e.response.dict())
-
-
-        
-        
-
-
-  
-  
-  
+    return orders
--- erpnext_ebay/sync_customers.py
+++ erpnext_ebay/sync_customers.py
@@ -1,108 +1,168 @@
+"""Functions to read through eBay orders and load customers and addresses"""
 
 from __future__ import unicode_literals
 import frappe
 from frappe import msgprint,_
 from frappe.utils import flt
-from datetime import date, datetime
 
-
-import requests.exceptions
-from ebay_requests import get_ebay_customers
 from ebay_requests import get_orders
-
-#from .utils import make_ebay_log
-
-''''
-Order Fields
-order.BuyerUserID, order.ShippingAddress.CityName, order.ShippingAddress.CountryName, order.ShippingAddress.Name, order.ShippingAddress.Phone, order.ShippingAddress.PostalCode, order.ShippingAddress.StateOrProvince, order.ShippingAddress.Street1, order.ShippingAddress.Street2])order.BuyerUserID, order.ShippingAddress.CityName, order.ShippingAddress.CountryName, order.ShippingAddress.Name, order.ShippingAddress.Phone, order.ShippingAddress.PostalCode, order.ShippingAddress.StateOrProvince, order.ShippingAddress.Street1, order.ShippingAddress.Street2
-'''
-
-
-
-num_days = 44
 
 
 @frappe.whitelist()
 def sync_new():
+    """Sync the Ebay database with the Frappe database."""
+
+    # Load orders from Ebay and load new customers
+    orders = get_orders()
+    for order in orders:
+        cust, address = extract_customer(order)
+        create_customer(cust, address, no_duplicates=True)
+
+
+def extract_customer(order):
+    """Process and order, and extract limited customer information
+
+    order - a single order entry from the eBay TradingAPI
+
+    Returns a tuple of two dictionarys.
+    The first dictionary is ready to create a Customer Doctype entry
+    The second dictionary is ready to create an Address Doctype entry
+    The second dictionary could be replaced by None if there is no address
+    """
+
+    ebay_user_id = order['BuyerUserID']
+
+    has_shipping_address = order['ShippingAddress']['Name'] is not None
+
+    if has_shipping_address:
+        ShippingAddress_Name = order['ShippingAddress']['Name'].decode("utf-8")
+        customer_name = ShippingAddress_Name
+    else:
+        ShippingAddress_Name = 'None'
+        customer_name = ebay_user_id
+
+    if has_shipping_address:
+        # Tidy up customer name, if entirely lower/upper case and not
+        # a single word (and if not an Ebay username)
+        if ((customer_name.islower() or customer_name.isupper()) and
+                ' ' in customer_name):
+            customer_name = customer_name.title()
+
+    customer_dict = {
+        "doctype": "Customer",
+        "customer_name" : customer_name,
+        "ebay_user_id": ebay_user_id,
+        "customer_group": _("Individual"),
+        "territory": _("All Territories"),
+        "customer_type": _("Individual")}
+
+    if has_shipping_address:
+        transactions = order['TransactionArray']['Transaction']
+        if len(transactions) == 1:
+            email_id = transactions[0]['Buyer']['Email']
+        else:
+            email_id = None
+        if email_id == 'Invalid Request':
+            email_id = None
+        postcode = order['ShippingAddress']['PostalCode']
+        country = order['ShippingAddress']['CountryName']
+        if postcode is not None and country=='United Kingdom':
+            postcode = sanitize_postcode(postcode)
+        address_dict = {"doctype": "Address",
+                        "address_title": customer_name,
+                        "address_type": _("Shipping"),
+                        "address_line1": order['ShippingAddress']['Street1'],
+                        "address_line2": order['ShippingAddress']['Street2'],
+                        "city": order['ShippingAddress']['CityName'],
+                        "state": order['ShippingAddress']['StateOrProvince'],
+                        "pincode": postcode,
+                        "country": order['ShippingAddress']['CountryName'],
+                        "phone": order['ShippingAddress']['Phone'],
+                        "email_id": email_id,
+                        "is_primary_address": 1,
+                        "customer_name": customer_name}
+    else:
+        address_dict = None
+
+    return customer_dict, address_dict
+
+
+def create_customer(customer_dict, address_dict, no_duplicates=True):
+    """Process an order and add the customer; add customer address
+
+    customer_dict - A dictionary ready to create a Customer doctype
+    address_dict - A dictionary ready to create an Address doctype (or None)
+    no_duplicates - Don't duplicate existing entries"""
+
+    # First test if the customer already exists
+    db_cust_name = None
+    if no_duplicates:
+        ebay_user_id = customer_dict['ebay_user_id']
+
+        db_cust_name = frappe.db.get_value(
+            "Customer",
+            filters={"ebay_user_id": ebay_user_id},
+            fieldname="name")
+        if db_cust_name is None:
+            msgprint('Adding a user: ' + ebay_user_id + ' : ' + customer_dict['customer_name'])
+        else:
+            msgprint('Not adding a user: ' + ebay_user_id + ' : ' + db_cust_name)
+
+        add_customer = db_cust_name is None
+    else:
+        add_customer = True
+
+    # Add customer if required
+    if add_customer:
+        frappe.get_doc(customer_dict).insert()
     
-    try:
-        orders = get_orders()
-        if int(orders.ReturnedOrderCountActual) > 0:
-            for order in orders.OrderArray.Order:
-                create_customer(order)
-                
-    except Exception, e:
-        msgprint('Problem getting orders')
+    # Now test if the address already exists
+    if address_dict is None:
+        add_address = False
+    elif no_duplicates:
+        keys = ('address_line1', 'address_line2', 'city', 'pincode')
+        filters = {}
+        for key in keys:
+            if address_dict[key] is not None:
+                filters[key] = address_dict[key]
         
-def create_customer(order):
-	#ebay_settings = frappe.get_doc("Shopify Settings", "Shopify Settings")
-	
-	#cust_name = (ebay_customer.get("first_name") + " " + (ebay_customer.get("last_name") \
-	#	and  ebay_customer.get("last_name") or "")) if ebay_customer.get("first_name")\
-	#	else ebay_customer.get("email")
-    
-    
-    try:
-        #custname = order.ShippingAddress.Name
-        #if custname.len <=0: custname = "BLANK"
-        msgprint(order.BuyerUserID)
-        customer = frappe.get_doc({
-		    "doctype": "Customer",
-		    "name": order.BuyerUserID,
-		    "customer_name" : order.ShippingAddress.Name,
-		    #"ebay_customer_id": ebay_customer.get("id"),
-		    #"sync_with_ebay": 1,
-		    "customer_group": _("Individual"),
-		    "territory": _("All Territories"),
-		    "customer_type": _("Individual")
-        }).insert()
-        
-        if customer:
-		    create_customer_address(customer, order)
-        
-        #ebay_customer_list.append(ebay_customer.get("id"))
+        db_address_name = frappe.db.get_value("Address",
+                                              filters=filters,
+                                              fieldname="name")
+        add_address = db_address_name is None
+    else:
+        add_address = True
+
+    # Add address if required
+    if add_address:
+        if db_cust_name is None:
+            # Find new customer 'name' field
+            ebay_user_id = customer_dict['ebay_user_id']
+
+            db_cust_name = frappe.db.get_value(
+                "Customer",
+                filters={"ebay_user_id": ebay_user_id},
+                fieldname="name")
+        address_dict['customer'] = db_cust_name
+        frappe.get_doc(address_dict).insert()
+
+    # Commit changes to database
+    if add_customer or add_address:
         frappe.db.commit()
-    
-    except Exception, e:
-        msgprint('Exception on customer creation')
-        if e.args[0] and e.args[0].startswith("402"):
-			raise e
-		#else:
-			#make_ebay_log(title=e.message, status="Error", method="create_customer", message=frappe.get_traceback(),
-			#	request_data=ebay_customer, exception=True)
 
-        
+    return None
 
 
-def create_customer_address(customer, order):
-	#for i, address in enumerate(ebay_customer.get("addresses")):
-	#	address_title, address_type = get_address_title_and_type(customer.customer_name, i)
-    address_title = customer.name
-    try:
-        msgprint(address_title)
-        msgprint(order.ShippingAddress.Street1)
-        #msgprint(order.TransactionArray.Transaction.Buyer.Email)
-        msgprint(customer.customer_name)
-        frappe.get_doc({
-			"doctype": "Address",
-			#"ebay_address_id": address.get("id"),
-			"address_title": address_title,
-			"address_type": _("Shipping"),
-			"address_line1": order.ShippingAddress.Street1,
-			"address_line2": order.ShippingAddress.Street2,
-			"city": order.ShippingAddress.CityName,
-			"state": order.ShippingAddress.StateOrProvince,
-			"pincode": order.ShippingAddress.PostalCode,
-			"country": order.ShippingAddress.CountryName,
-			"phone": order.ShippingAddress.Phone,
-			#"email_id": order.TransactionArray.Transaction.Buyer.Email,
-			"customer": customer.name,
-			"customer_name":  customer.customer_name
-	    }).insert()
-    except Exception, e:
-        msgprint('Exception on address creation')
-		#make_ebay_log(title=e.message, status="Error", method="create_customer_address", message=frappe.get_traceback(),
-		#	request_data=ebay_customer, exception=True)
-        
-        
-        
+def sanitize_postcode(in_postcode):
+    """Take a UK postcode and tidy it up (spacing and capitals)"""
+
+    postcode = in_postcode.strip().replace(' ', '').upper()
+    if (6 > len(postcode) > 8):
+        print(in_postcode)
+        raise ValueError('Unknown postcode type!')
+        return in_postcode
+
+    # A single space always precedes the last three characters of a UK postcode
+    postcode = postcode[:-3] + ' ' + postcode[-3:]
+
+    return postcode
