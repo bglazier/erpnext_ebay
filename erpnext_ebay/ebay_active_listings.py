35eac882c80e4db31f3694a644bd9a56da92c5c4
Ben Glazier <ben@benjaminglazier.com>
2018-03-20 16:14:15 -0700

    added get active option

--- erpnext_ebay/ebay_active_listings.py
+++ erpnext_ebay/ebay_active_listings.py
@@ -35,15 +35,55 @@ def better_print(*args, **kwargs):
 print = better_print
 
 
-
-
 @frappe.whitelist()
 def show_list():
 	
 	create_ebay_listings_table()
 	
 	page = 1
-	listings_dict = find_listings(page)
+	listings_dict = get_myebay_selling_request(page)
+	pages = int(listings_dict['PaginationResult']['TotalNumberOfPages'])
+	#timestamp = listings_dict['Timestamp']
+	
+	while pages >= page:
+
+		for item in listings_dict['ActiveList']['ItemArray']['Item']:
+			ebay_id = item['ItemID']
+			qty = item['Quantity']
+			try:
+				sku = item['SKU']
+			except:
+				sku = ''
+			price = item['BuyItNowPrice']['value']
+			#THSI IS 0    	print(item['BuyItNowPrice']['value'])
+			#Example: {'_currencyID': 'USD', 'value': '0.0'}   print(item['BuyItNowPrice'])
+			curr_price = item['SellingStatus']['CurrentPrice']['value']
+			currency = item['SellingStatus']['CurrentPrice']['_currencyID']  # or ['Currency']
+			print(currency)
+			#converted_price = item['ListingDetails]['ConvertedBuyItNowPrice']['value']
+			#description = item['Description']
+			hit_count = 0 #item['HitCount']
+			watch_count = 0 #item['WatchCount']
+			question_count = 0 #item['TotalQuestionCount']
+			site = item['Site']
+			#title = item['Title']
+			#conv_title = title.encode('ascii', 'ignore').decode('ascii')
+			#new_title = MySQLdb.escape_string(conv_title)
+			insert_ebay_listing(sku, ebay_id, qty, curr_price, site, hit_count, watch_count, question_count)
+
+		page += 1
+		if pages >= page:
+			listings_dict = get_myebay_selling_request(page)
+		else:
+			break
+
+@frappe.whitelist()
+def show_listOLD():
+	
+	create_ebay_listings_table()
+	
+	page = 1
+	listings_dict = get_seller_list(page)
 	pages = int(listings_dict['PaginationResult']['TotalNumberOfPages'])
 	#timestamp = listings_dict['Timestamp']
 	
@@ -75,13 +115,14 @@ def show_list():
 
 		page += 1
 		if pages >= page:
-			listings_dict = find_listings(page)
+			listings_dict = get_seller_list(page)
 		else:
 			break
 
 
-	
-def find_listings(page):
+
+
+def get_myebay_selling_request(page):
 	
 	# python -c "import certifi; print certifi.old_where()"
 	#os.environ['REQUESTS_CA_BUNDLE'] = '/usr/local/lib/python2.7/dist-packages/certifi/cacert.pem'	
@@ -89,8 +130,47 @@ def find_listings(page):
 	try:
 		api_trading = Trading(config_file='/home/frappe/ebay.yaml', warnings=True, timeout=20)
 		
+		#datetime.today().format('yyyy-mm-ddThh:mm:ss')
+		
 		api_request = {
-			'EndTimeFrom': "2018-01-30T00:00:00",
+			"ActiveList":{"Include": True},
+			"Pagination": {
+				"EntriesPerPage": 100,
+				"PageNumber": page
+			},
+		'DetailLevel': 'ReturnAll'
+		}
+		
+		
+		# activelist = api.execute('GetMyeBaySelling', {'ActiveList': True,'DetailLevel': 'ReturnAll','PageNumber': page})
+
+		api_trading.execute('GetMyeBaySellingRequest', api_request)
+		products = api_trading.response.dict()
+	
+
+	except ConnectionError as e:
+		print(e)
+		print(e.response.dict())
+		raise e
+		
+	return products
+
+
+
+
+	
+def get_seller_list(page):
+	
+	# python -c "import certifi; print certifi.old_where()"
+	#os.environ['REQUESTS_CA_BUNDLE'] = '/usr/local/lib/python2.7/dist-packages/certifi/cacert.pem'	
+	
+	try:
+		api_trading = Trading(config_file='/home/frappe/ebay.yaml', warnings=True, timeout=20)
+		
+		#datetime.today().format('yyyy-mm-ddThh:mm:ss')
+		
+		api_request = {
+			'EndTimeFrom': "2018-03-22T00:00:00",
 			'EndTimeTo': "2018-05-01T00:00:00",
 			"Pagination": {
 				"EntriesPerPage": 100,
