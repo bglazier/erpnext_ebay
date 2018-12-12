39232795eaf73f5b619e79acc0372645120b4343
Andrew McLeod <amcleod@astro.ex.ac.uk>
2018-12-12 10:42:00 +0000

    Little bit of tidying up. sync_customers => sync_orders. Some buttons were custom fields that should have been eBay Manager core fields. Renaming some buttons and functions. The primary aim of (sync_customers=>sync_orders) is to generate sales invoices; creating Customer records is only a part of this. Hence moving to sync_orders is considered justified. The old sync_orders (which just contained unused code for creating sales_orders, sales invoices, etc) is temporarily moved pending deletion once it has been stripped of any value.

--- erpnext_ebay/erpnext_ebay/doctype/ebay_manager/ebay_manager.js
+++ erpnext_ebay/erpnext_ebay/doctype/ebay_manager/ebay_manager.js
@@ -2,45 +2,38 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('eBay Manager', {
-    refresh: function(frm) {},
-
-
-    customer_sync: function(frm) {
+    sync_orders_button: function(frm) {
         frappe.call({
-            method: "erpnext_ebay.sync_customers.sync",
+            method: "erpnext_ebay.sync_orders.sync",
             args: {},
-            callback: function(r){}
-                //cur_frm.reload_doc();
+            freeze: true,
+            freeze_message: "Syncing eBay customers and orders; this make take some time..."
         });
     },
 
+    sync_categories_button: function(frm) {
+        frappe.call({
+            method: "erpnext_ebay.ebay_categories.category_sync",
+            args: {},
+            freeze: true,
+            freeze_message: "Loading eBay categories; this make take some time..."
+        });
+    },
 
-    create_garagesale: function(frm) {
+    price_sync_button: function(frm) {
+        frappe.call({
+            method: "erpnext_ebay.ebay_price_sync.price_sync",
+            args: {}
+        });
+    },
+
+    create_garagesale_button: function(frm) {
         if (confirm('Have you relisted ended eBay listings?')) {
             // Run it!
             frappe.call({
                 method: "erpnext_ebay.garage_sale.run_cron_create_xml",
-                args: {},
-                callback: function(r){}
+                args: {}
             });
         }
     },
-
-
-    price_sync: function(frm) {
-        frappe.call({
-            method: "erpnext_ebay.ebay_price_sync.price_sync",
-            args: {},
-            callback: function(r) {}
-        });
-    },
-
-
-    category_sync: function(frm) {
-        frappe.call({
-            method: "erpnext_ebay.ebay_categories.category_sync",
-            args: {},
-            callback: function(r){}
-        });
-    }
 });
