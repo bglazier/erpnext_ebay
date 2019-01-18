5c52852e6949335d94a987d85e8bdc34901a7266
Ben Glazier <ben@benjaminglazier.com>
2019-01-18 12:34:40 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+import re, ast
 
 version = '0.0.1'
-requirements = parse_requirements("requirements.txt", session="")
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 setup(
 	name='erpnext_ebay',
@@ -14,6 +16,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
+	#dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
