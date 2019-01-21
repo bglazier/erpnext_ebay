83d3def4a529c814ba276b02560b7b0143c3ff8a
Andrew McLeod <amcleod@astro.ex.ac.uk>
2019-01-21 13:06:57 +0000

    Update setup.py and __init__.py to match new-site output

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@
 from setuptools import setup, find_packages
 import re, ast
 
-version = '0.0.1'
-
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
+
+# get version from __version__ variable in erpnext_ebay/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('erpnext_ebay/__init__.py', 'rb') as f:
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='erpnext_ebay',
@@ -17,5 +22,4 @@ setup(
 	zip_safe=False,
 	include_package_data=True,
 	install_requires=install_requires
-	#dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
