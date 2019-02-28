f530edb42462711a898a4dffdc6779d3acd8e818
Andrew McLeod <andrewclimbing@gmail.com>
2019-02-28 13:15:19 +0000

    feat: Fix add of new Doctypes for Online Selling APIs

---
+++ erpnext_ebay/erpnext_ebay/doctype/online_selling_platform/test_online_selling_platform.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Ben Glazier and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+
+class TestOnlineSellingType(unittest.TestCase):
+    pass
