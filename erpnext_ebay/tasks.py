40b78b23072841cb7926d06a9d37f5a7cdd817ab
Andrew McLeod <andrewclimbing@gmail.com>
2021-01-14 09:07:32 +0000

    fix: Add multiple_error_sites for daily eBay update

--- erpnext_ebay/tasks.py
+++ erpnext_ebay/tasks.py
@@ -16,7 +16,8 @@ def hourly():
 
 def daily():
     enqueue('erpnext_ebay.ebay_active_listings.update_ebay_data',
-            queue='long', job_name='Update eBay Data')
+            queue='long', job_name='Update eBay Data',
+            multiple_error_sites=['UK'])
     enqueue('erpnext_ebay.ebay_categories.category_sync',
             queue='long', job_name='eBay Category Sync')
 
