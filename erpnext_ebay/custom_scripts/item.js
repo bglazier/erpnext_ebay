1c8e73bea87b85bd88913aa77decb99949d16cca
Andrew McLeod <andrewclimbing@gmail.com>
2021-02-16 10:43:39 +0000

    fix: Collapse 'Online Selling Items'

--- erpnext_ebay/custom_scripts/item.js
+++ erpnext_ebay/custom_scripts/item.js
@@ -11,6 +11,7 @@ function get_online_selling_items(frm, item_code) {
     }).then(({message: osi_list}) => {
         // Add Online Selling Items, but don't dirty the form
         if (!(osi_list && osi_list.length)) {
+            frm.get_field('online_selling_section').collapse(true);
             frappe.ui.form.trigger("Item", "online_selling_async_complete");
             return;
         }
@@ -23,6 +24,7 @@ function get_online_selling_items(frm, item_code) {
             child.idx = index + 1;
         });
         frm.refresh_field('online_selling_items');
+        frm.get_field('online_selling_section').collapse(false);
         frm.doc.__unsaved = unsaved;
         // Delay the trigger so that updates have occurred first
         // following a save or similar (e.g. from after_save event)
