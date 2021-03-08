1e55d82bee4360608d5053ac05c8a62f57d72bf7
Andrew McLeod <andrewclimbing@gmail.com>
2021-03-08 11:15:52 +0000

    fix: Use parameter for maximum number of eBay images

--- erpnext_ebay/custom_methods/website_slideshow_methods.py
+++ erpnext_ebay/custom_methods/website_slideshow_methods.py
@@ -3,11 +3,14 @@
 
 import frappe
 
+MAX_EBAY_IMAGES = 12
+
 
 def website_slideshow_validate(doc, _method):
     """On Website Slideshow validate docevent."""
 
-    if doc.number_of_ebay_images > 12:
-        frappe.throw('Number of eBay images must be 12 or fewer!')
+    if doc.number_of_ebay_images > MAX_EBAY_IMAGES:
+        frappe.throw(
+            f'Number of eBay images must be {MAX_EBAY_IMAGES} or fewer!')
     if doc.number_of_ebay_images < 1:
         frappe.throw('Number of eBay images must be 1 or greater!')
