cc73514ccbd310fac9ad1f88b44c68af53aab4b2
Andrew McLeod <andrewclimbing@gmail.com>
2021-10-20 20:39:32 +0100

    fix(eBay Pending Orders): Display last_modified and created_time as Data not Datetime Otherwise, you get a local timezone appended (which is inaccurate)

--- erpnext_ebay/erpnext_ebay/doctype/ebay_pending_order/ebay_pending_order.js
+++ erpnext_ebay/erpnext_ebay/doctype/ebay_pending_order/ebay_pending_order.js
@@ -2,4 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('eBay Pending Order', {
+    refresh(frm) {
+        // Set fieldtype to 'Data' for last_modified and created_time
+        // so that we don't display the local timezone
+        frm.set_df_property('last_modified', 'fieldtype', 'Data');
+        frm.set_df_property('created_time', 'fieldtype', 'Data');
+    }
 });
