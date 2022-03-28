5ecd01bb276faf11ce940e856409109de42eb644
Andrew McLeod <andrewclimbing@gmail.com>
2022-03-28 09:24:44 +0100

    fix(sync_mp_transactions): Ignore 'FUNDS_PROCESSING' transfers

--- erpnext_ebay/sync_mp_transactions.py
+++ erpnext_ebay/sync_mp_transactions.py
@@ -267,6 +267,9 @@ def sync_mp_transactions(num_days=None, not_today=False,
             # (transactions do not appear to progress from
             # Payout to Completed)
             pass
+        elif t_status == 'FUNDS_PROCESSING':
+            # This transaction is incomplete: don't add yet
+            continue
         elif t_status == 'FAILED':
             # This transaction failed, and does not need to be added
             continue
