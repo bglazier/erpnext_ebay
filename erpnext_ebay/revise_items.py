e87232ebbf6fdc9b44b929bf57f39a8620486b0a
Andrew McLeod <andrewclimbing@gmail.com>
2022-12-19 17:45:16 +0000

    fix(revise_items): Add hooks for eBay items being revised or ended

--- erpnext_ebay/revise_items.py
+++ erpnext_ebay/revise_items.py
@@ -21,9 +21,10 @@ class eBayPartialFailure(frappe.ValidationError):
     pass
 
 
-def revise_ebay_prices(price_data, print=print, **kwargs):
+def revise_ebay_prices(price_data, print=print, item_codes=None, **kwargs):
     """Revises multiple eBay prices. Attempts to pack price updates into as few
     ReviseInventoryStatus calls as possible.
+    Accepts a list of item codes which should match the length of price_data.
     Accepts a list of tuples, each of which contains:
       - ebay_id
       - new_price
@@ -34,17 +35,24 @@ def revise_ebay_prices(price_data, print=print, **kwargs):
         print('No prices to update!')
         return
 
+    if not item_codes:
+        item_codes = [None] * len(price_data)
+    elif len(price_data) != len(item_codes):
+        frappe.throw('price_data and item_codes must be the same length!')
+
     item_data = [
         (ebay_id, new_price, None)
         for (ebay_id, new_price, *_) in price_data
     ]
 
-    revise_ebay_inventory(item_data, print=print, **kwargs)
+    revise_ebay_inventory(item_data, print=print,
+                          item_codes=item_codes, **kwargs)
 
 
-def revise_ebay_quantities(qty_data, print=print, **kwargs):
+def revise_ebay_quantities(qty_data, print=print, item_codes=None, **kwargs):
     """Revises multiple eBay quantities. Attempts to pack quantity updates
     into as few ReviseInventoryStatus calls as possible.
+    Accepts a list of item codes which should match the length of qty_data.
     Accepts a list of tuples, each of which contains:
       - ebay_id
       - new_qty
@@ -55,28 +63,40 @@ def revise_ebay_quantities(qty_data, print=print, **kwargs):
         print('No quantities to update!')
         return
 
+    if not item_codes:
+        item_codes = [None] * len(qty_data)
+    elif len(qty_data) != len(item_codes):
+        frappe.throw('qty_data and item_codes must be the same length!')
+
     revise_listings = []
+    revise_item_codes = []
     end_listings = []
-    for ebay_id, new_qty, *_ in qty_data:
+    end_item_codes = []
+    for (ebay_id, new_qty, *_), item_code in zip(qty_data, item_codes):
         if new_qty == 0:
             end_listings.append((ebay_id, 'NotAvailable'))
+            end_item_codes.append(item_code)
         else:
             revise_listings.append((ebay_id, None, new_qty))
+            revise_item_codes.append(item_code)
 
     # First revise listing quantities
     if revise_listings:
-        revise_ebay_inventory(revise_listings, print=print, **kwargs)
+        revise_ebay_inventory(revise_listings, print=print,
+                              item_codes=revise_item_codes, **kwargs)
 
     # Then end listings
     if end_listings:
-        end_ebay_listings(end_listings, print=print, **kwargs)
+        end_ebay_listings(end_listings, print=print,
+                          item_codes=end_item_codes, **kwargs)
 
 
-def revise_ebay_inventory(item_data, print=print, error_log=None,
-                          retries=1, **kwargs):
+def revise_ebay_inventory(item_data, print=print, item_codes=None,
+                          error_log=None, retries=1, **kwargs):
     """Revises multiple eBay prices and quantities. Attempts to pack
     price updates into as few ReviseInventoryStatus calls as possible.
     Accepts a list of tuples of the form (ebay_id, price, qty).
+    Accepts a list of item codes being revised.
     If either price or qty is None, the price or qty, respectively,
     is not changed.
     Tries each transaction 'retries' times.
@@ -101,6 +121,14 @@ def revise_ebay_inventory(item_data, print=print, error_log=None,
     n_items = len(items)
     n_chunks = ((n_items - 1) // CHUNK_SIZE) + 1  # Number of chunks of 4 items
     print('n_chunks: ', n_chunks)
+
+    # Filter out empty item_codes
+    item_codes = [x for x in (item_codes or []) if x]
+    if item_codes:
+        run_before_hooks = frappe.get_hooks(
+            "erpnext_ebay_before_revise_ebay_inventory") or []
+        for method_name in run_before_hooks:
+            frappe.get_attr(method_name)(item_codes)
 
     if n_items == 0:
         print('No items to update!')
@@ -138,7 +166,7 @@ def revise_ebay_inventory(item_data, print=print, error_log=None,
 
 
 @frappe.whitelist()
-def client_revise_ebay_item(item_data):
+def client_revise_ebay_item(item_data, item_code=None):
     """Revise an item's price and/or qty from the JS front-end.
     Will end a listing if the qty is set to zero.
     """
@@ -148,14 +176,18 @@ def client_revise_ebay_item(item_data):
         frappe.throw('Need write permissions on eBay Manager!',
                      frappe.PermissionError)
 
-    item_data = json.loads(item_data)
+    if isinstance(item_data, str):
+        item_data = json.loads(item_data)
+    if not isinstance(item_data, dict):
+        frappe.throw('Invalid format for item_data!')
+
     qty = item_data.get('qty')
     if qty == 0:
         item = (item_data['ebay_id'], 'NotAvailable')
-        end_ebay_listings([item])
+        end_ebay_listings([item], item_codes=[item_code])
     else:
         item = item_data['ebay_id'], item_data.get('price'), qty
-        revise_ebay_inventory([item])
+        revise_ebay_inventory([item], item_codes=[item_code])
 
 
 def relist_ebay_item(ebay_id, item_dict=None):
@@ -175,14 +207,13 @@ def client_relist_ebay_item(ebay_id, item_dict=None):
 
     if isinstance(item_dict, str):
         item_dict = json.loads(item_dict)
-
     if not isinstance(item_dict, dict):
         frappe.throw('Invalid format for item_dict!')
 
     relist_item(ebay_id, item_dict=item_dict)
 
 
-def end_ebay_listings(listings, print=print, **kwargs):
+def end_ebay_listings(listings, print=print, item_codes=None, **kwargs):
     """Ends a number of eBay listings.
 
     Arguments:
@@ -202,6 +233,15 @@ def end_ebay_listings(listings, print=print, **kwargs):
         {'ItemID': ebay_id, 'EndingReason': reason}
         for ebay_id, reason in listings
     ]
+
+
+    # Filter out empty item_codes
+    item_codes = [x for x in (item_codes or []) if x]
+    if item_codes:
+        run_before_hooks = frappe.get_hooks(
+            "erpnext_ebay_before_end_ebay_listings") or []
+        for method_name in run_before_hooks:
+            frappe.get_attr(method_name)(item_codes)
 
     prev_percent = -1000.0
     n_items = len(items)
@@ -249,7 +289,7 @@ def end_ebay_listings(listings, print=print, **kwargs):
 
 
 @frappe.whitelist()
-def client_end_ebay_listings(ebay_ids):
+def client_end_ebay_listings(ebay_ids, item_codes=None):
     """End listings from the JS front-end."""
 
     # Whitelisted function; check permissions
@@ -262,6 +302,11 @@ def client_end_ebay_listings(ebay_ids):
     if not isinstance(ebay_ids, list):
         frappe.throw('Invalid ebay_ids format!')
 
+    if isinstance(item_codes, str):
+        item_codes = json.loads(item_codes)
+    if not isinstance(item_codes, list):
+        frappe.throw('Invalid item_codes format!')
+
     listings = [(x, 'NotAvailable') for x in ebay_ids]
 
-    end_ebay_listings(listings)
+    end_ebay_listings(listings, item_codes=item_codes)
