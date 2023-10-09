003da991a34faf1966dffec4c79d6dc076646360
Andrew McLeod <andrewclimbing@gmail.com>
2023-10-09 18:53:33 +0100

    fix(sync_orders_rest): Simplify refund bit (after finding documentation)

--- erpnext_ebay/sync_orders_rest.py
+++ erpnext_ebay/sync_orders_rest.py
@@ -1402,17 +1402,11 @@ def create_return_sales_invoice(order_dict, order, changes, print_func=None):
     if not order['payment_summary']['refunds']:
         frappe.throw(f'Order {ebay_order_id} missing refund info?',
                      exc=ErpnextEbaySyncError)
-    # Search for new refund states (undocumented API feature)
-    refunds = order['payment_summary']['refunds']
-    for refund in refunds:
-        if refund.get('refund_status') not in ('REFUNDED', 'FAILED'):
-            frappe.throw(
-                f'Order {ebay_order_id} has unknown refund '
-                + f'status {refund.get("refund_status")}',
-                exc=ErpnextEbaySyncError
-            )
     # Filter out refunds with refund_state 'FAILED'
-    refunds = [x for x in refunds if x.get('refund_status') != 'FAILED']
+    refunds = [
+        x for x in order['payment_summary']['refunds']
+        if x.get('refund_status') == 'REFUNDED'
+    ]
     if len(refunds) > 1:
         frappe.throw(f'Order {ebay_order_id} has multiple refunds!',
                      exc=ErpnextEbaySyncError)
