7a47dfbe654ba4005db1f31018437724e33e07bb
Andrew McLeod <andrewclimbing@gmail.com>
2026-07-07 09:38:02 +0100

    fix(eBay sync log): Add clear_old_logs support

--- erpnext_ebay/erpnext_ebay/doctype/ebay_sync_log/ebay_sync_log.py
+++ erpnext_ebay/erpnext_ebay/doctype/ebay_sync_log/ebay_sync_log.py
@@ -2,10 +2,20 @@
 # Copyright (c) 2015, Ben Glazier and contributors
 # For license information, please see license.txt
 
-
-
+import frappe
 from frappe.model.document import Document
 
 
 class eBaysynclog(Document):
-    pass
+
+    @staticmethod
+    def clear_old_logs(days=120):
+        """Allows use in Log Settings for automated log clearing."""
+        from frappe.query_builder import Interval
+        from frappe.query_builder.functions import Now
+
+        table = frappe.qb.DocType("eBay sync log")
+        frappe.db.delete(
+            table,
+            filters=(table.ebay_sync_datetime < (Now() - Interval(days=days)))
+        )
