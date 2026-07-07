9aa107106a9c85f3f987feda6b2c9e0cab7ed87f
Andrew McLeod <andrewclimbing@gmail.com>
2026-07-07 10:06:52 +0100

    fix(tasks): Remove broken eBay sync log clearing

--- erpnext_ebay/tasks.py
+++ erpnext_ebay/tasks.py
@@ -24,8 +24,6 @@ def daily():
     enqueue('erpnext_ebay.erpnext_ebay.doctype.ebay_shipping_carrier.'
             + 'ebay_shipping_carrier.sync_shipping_carriers',
             queue='long', job_name='eBay Shipping Carrier Sync')
-    enqueue('erpnext_ebay.tasks.clear_old_ebay_sync_logs',
-            queue='long', job_name='Clear old eBay sync logs')
 
 
 def weekly():
@@ -34,19 +32,3 @@ def weekly():
 
 def monthly():
     pass
-
-
-def clear_old_ebay_sync_logs():
-    """Delete any eBay sync logs older than 120 days."""
-
-    DAYS = 120
-
-    today = frappe.utils.getdate()
-
-    cut_off_date = today - datetime.timedelta(days=DAYS)
-
-    frappe.db.sql("""
-        DELETE LOW_PRIORITY QUICK
-        FROM `tabeBay sync log`
-        WHERE creation < %(cut_off_date)s;
-    """, {'cut_off_date': cut_off_date})
